e2d16cb071ca6f43a65e9c9db2e47147518507db
Nikhil Vas <nikhilvas123@gmail.com>
2021-08-22 19:55:57 +0530

    Implemented isAnyMiddleEdgePieceInvalid

--- 3_Implementation/src/layer_second.c
+++ 3_Implementation/src/layer_second.c
@@ -26,3 +26,48 @@ boolean isAnyUpEdgePieceValid(){
     else 
         return false;
 }
+
+//checks if the edge piece at 001 is valid 
+boolean isUpEdgePieceValid(){
+    char* upColor = getUpMidColor();
+    char* frontColor = getFrontColor(0,0,1);
+    char* frontUpColor = getUpColor(0,0,1);
+    
+    boolean isFrontPieceValid = !((strcmp(frontColor,(upColor))==0)?true:false) && !((strcmp(frontUpColor,(upColor))==0)?true:false);
+    
+    if(isFrontPieceValid)
+        return true;
+    else 
+        return false;
+}
+
+//checks if any edge piece in the middle layer is invalid/(incorrectly placed)
+boolean isAnyMiddleEdgePieceInvalid(){
+    int j=1;
+    
+    char* frontColor = getFrontMidColor();
+    char* leftColor = getLeftMidColor();
+    char* backColor = getBackMidColor();
+    char* rightColor = getRightMidColor();
+    
+    char* frontFMLColor = getFrontColor(0, j, 0);
+    char* leftFMLColor = getLeftColor(0, j, 0);
+    char* leftBMLColor = getLeftColor(2, j, 0);
+    char* backBMLColor = getBackColor(2, j, 0);
+    char* backBMRColor = getBackColor(2, j, 2);
+    char* rightBMRColor = getRightColor(2, j, 2);
+    char* rightFMRColor = getRightColor(0, j, 2);
+    char* frontFMRColor = getFrontColor(0, j, 2);
+    
+    boolean isFMLValid = ((strcmp(frontFMLColor,(frontColor))==0)?true:false) && ((strcmp(leftFMLColor,(leftColor))==0)?true:false);
+    boolean isBMLValid = ((strcmp(leftBMLColor,(leftColor))==0)?true:false) && ((strcmp(backBMLColor,(backColor))==0)?true:false);
+    boolean isBMRValid = ((strcmp(backBMRColor,(backColor))==0)?true:false) && ((strcmp(rightBMRColor,(rightColor))==0)?true:false);
+    boolean isFMRValid = ((strcmp(rightFMRColor,(rightColor))==0)?true:false) && ((strcmp(frontFMRColor,(frontColor))==0)?true:false);
+    
+    if(!isFMLValid || !isBMLValid || !isBMRValid || !isFMRValid){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
